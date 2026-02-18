4ed49255b83a0b2f91697fe6f18639813206d2d5
Rashmith <rashmithmr@gmail.com>
2026-02-18 22:36:56 +0530

    handled player connection in server

--- miniProject3/server/game.go
+++ miniProject3/server/game.go
@@ -1,40 +1,78 @@
 package main
 
 import (
+	"bufio"
 	"math/rand"
 	"net"
-	"strings"
 	"strconv"
+	"strings"
 	"sync"
 )
-type Game struct{
-	secretNo string
-	Players [] Player
-	Winner *Player
+
+type Game struct {
+	secretNo   string
+	Players    []Player
+	Winner     *Player
 	isGameover bool
-	Mutex sync.Mutex
+	lock       sync.Mutex
 }
 
-type Player struct{
-	conn net.Conn
-	Id int
+type Player struct {
+	conn       net.Conn
+	Id         int
 	guessCount int
 	isFinished bool
 }
 
-
-func SectNumGenrator() string{
+func SectNumGenrator() string {
 	var secret string
-	for len(secret)<4{
-		num:=rand.Intn(10)
+	for len(secret) < 4 {
+		num := rand.Intn(10)
 		strNum := strconv.Itoa(num)
-		if !strings.Contains(secret,strNum){
-			secret+=strNum
+		if !strings.Contains(secret, strNum) {
+			secret += strNum
 		}
 	}
 	return secret
 }
 
-func handlePlayer(game *Game,player *Player){
+func validGuess(guess string) bool {
+	if len(guess) != 4 {
+		return false
+	}
+	seen := make(map[byte]bool)
+
+	for i := range guess {
+		if guess[i] < '0' || guess[i] > '9' {
+			return false
+		}
+
+		if seen[guess[i]] {
+			return false
+		}
+
+		seen[guess[i]] = true
+	}
+	return true
+}
+
+func handlePlayer(game *Game, player *Player) {
+	conec := player.conn
+	reader := bufio.NewReader(conec)
+	for {
+		guess, err := reader.ReadString('\n')
+		if err != nil {
+			conec.Write([]byte(err.Error()))
+			return
+		}
+		guess = strings.TrimSpace(guess)
+		if !validGuess(guess) {
+			conec.Write([]byte("Enter valid guess"))
+			continue
+		}
+		game.lock.Lock()
+		//call fun to check guess and retrn bulls and cows
+
+	}
 
 }
